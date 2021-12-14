5e23ac8c1a89ee5896d71c8491923b912f42a777
Adam Bartůšek <bartusek.a@seznam.cz>
2021-12-14 23:04:34 +0100

    nacteni souboru, kontejnery

--- ukol3.py
+++ ukol3.py
@@ -1,2 +1,31 @@
 import json
+from pyproj import Transformer
+
+ADRESY = "adresy.geojson"
+KONTEJNERY = "kontejnery.geojson"
+
+def nacti_soubor(nazev):
+    with open(nazev, "r", encoding="utf8") as infile:
+        return json.load(infile)["features"]
+
+
+kontejnery = nacti_soubor("kontejnery.geojson")
+
+verejne_kont = []
+for a in kontejnery:
+    if a['properties']['PRISTUP'] == 'volně':
+        verejne_kont.append(a)
+
+print(type(verejne_kont))
+print(len(verejne_kont))
+
+
+"""def prevod_souradnic(x,y):
+    wgs2jtsk = Transformer.from_crs(4326,5514)
+    return wgs2jtsk.transform(x,y)"""
+
+
+
+
+
 
