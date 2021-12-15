cf0d4f1c937ef44838a1c3a09e94b601e6ce6050
Adam Bartůšek <bartusek.a@seznam.cz>
2021-12-15 14:59:31 +0100

    zjisteni souradnic adresnich bodu

--- ukol3.py
+++ ukol3.py
@@ -24,15 +24,22 @@ print(verejne_kont)"""
 
 def prevod_souradnic(x,y):
     wgs2jtsk = Transformer.from_crs(4326,5514)
-    return wgs2jtsk.transform(x,y)
+    return wgs2jtsk.transform(y,x)
 
 def vzdalenost(x1,y1,x2,y2):
     return sqrt((x2-x1)**2 + (y2-y1)**2)
 
 adresy = nacti_soubor("adresy.geojson")
 
-print(type(adresy))
-print(adresy)
+#print(type(adresy))
+#print(adresy)
+
+for b in adresy:
+    b["geometry"]["coordinates_jtsk"] = list(prevod_souradnic(*b["geometry"]["coordinates"]))
+    x1 = b["geometry"]["coordinates_jtsk"][0]
+    y1 = b["geometry"]["coordinates_jtsk"][1]
+    print(x1,y1)
+    
 
 
 
