1689a9123911e22dd7707c187d8456887a753bfe
Adam Bartůšek <bartusek.a@seznam.cz>
2021-12-15 16:31:59 +0100

    spocitany vzdalenosti a vytvoren seznam

--- ukol3.py
+++ ukol3.py
@@ -22,11 +22,11 @@ print(len(verejne_kont))
 print(verejne_kont)"""
 
 
-def prevod_souradnic(x,y):
+def preved_souradnice(x,y):
     wgs2jtsk = Transformer.from_crs(4326,5514)
     return wgs2jtsk.transform(y,x)
 
-def vzdalenost(x1,y1,x2,y2):
+def spocti_vzdalenost(x1,y1,x2,y2):
     return sqrt((x2-x1)**2 + (y2-y1)**2)
 
 adresy = nacti_soubor("adresy.geojson")
@@ -34,14 +34,31 @@ adresy = nacti_soubor("adresy.geojson")
 #print(type(adresy))
 #print(adresy)
 
+vzdalenost_min = 100000
+soucet_vzdalenosti = 0
+vzdalenosti = []
 for b in adresy:
     id_adr = b["id"]
     
-    b["geometry"]["coordinates_jtsk"] = list(prevod_souradnic(*b["geometry"]["coordinates"]))
+    b["geometry"]["coordinates_jtsk"] = list(preved_souradnice(*b["geometry"]["coordinates"]))
     x1 = b["geometry"]["coordinates_jtsk"][0]
     y1 = b["geometry"]["coordinates_jtsk"][1]
+    #print(b["properties"]["addr:street"])
+    for c in verejne_kont:
+        x2 = c["geometry"]["coordinates"][0]
+        y2 = c["geometry"]["coordinates"][1]
+        #print(c["properties"]["STATIONNAME"])
+        vzdalenost = spocti_vzdalenost(x1,y1,x2,y2)
+        #print(vzdalenost)
+        if vzdalenost < vzdalenost_min:
+            vzdalenost_min = vzdalenost
+    #print(vzdalenost_min)
+    vzdalenosti.append(vzdalenost_min)
+    vzdalenost_min = 100000
 
-    print(id_adr,x1,y1)
+
+print(vzdalenosti)
+        
     
 
     
